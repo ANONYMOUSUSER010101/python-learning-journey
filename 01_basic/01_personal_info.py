68cbfdc31d485fad8729f9fd49c6f44d77d62845
ANONYMOUSUSER010101 <jelabong16@gmail.com>
2026-09-13 02:06:26 +0800

    Add Challenge 01 - Personal Information

---
+++ 01_basic/01_personal_info.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+section = input("Enter your section: ")
+
+print("Name:", name)
+print("Age:", age)
+print("Section:", section)
